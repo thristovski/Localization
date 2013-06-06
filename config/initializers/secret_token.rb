314c33311a5b774f52f790f8a4998338fb933f3f
unknown <Tome.Hristovski@tome_h.interworks.local>
2013-06-06 16:41:51 +0200

    Added Localization

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = 'd8110b108890dd40f400c73dbd8cf7e57410be11cafd4969b18321b1262f7ee5d1ef2679d5d73a40feb25e44454b3eb6b9ae93bdd30bb8d9761e54bd6a004ffa'
